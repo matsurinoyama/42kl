b2722952ece9b7771fbf3cf45273831a10ced313
Azhan Rizan <matsurinoyama@pm.me>
2023-02-08 21:26:34 +0800

    Fixed spec issues with ex07 for shell01, should work within spec now.

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,3 +1,3 @@
-cat /etc/passwd | sed 's/:.*//' | rev | sort -r |
+cat /etc/passwd | sed 's/:.*//' | rev | sort -r | sed "$FT_LINE1"','"$FT_LINE2"'!d' |
 awk '{ if (NR>1) { { NR % 2 == 0 } print } else print }' |
 tr '\n' ' ' | sed -e 's/ /, /g' -e 's/, $/./'
--- shell01/ex08/exportFT_eg1.sh
+++ shell01/ex08/exportFT_eg1.sh
@@ -5,3 +5,5 @@ str4='\'
 export FT_NBR1=$str1$str2$str3$str4
 #              \'?  "\"  '    \
 export FT_NBR2="rcrdmddd"
+
+echo "FT_NBRs patched successfully!"
