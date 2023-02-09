3d215fcefc3ff97e3341cbddc02263ca2af23c8f
Muhammad Azhan Bin Fadzlan Rizan <mbin-fad@u81z02s02.42kl.edu.my>
2023-02-09 17:21:50 +0800

    Fixed some exercises from shell01. Will fix further some other time

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,4 +1,4 @@
 cat /etc/passwd | sed 's/:.*// ; /\#.*/d' |
-awk '{ if (NR>1) { if (NR % 2 == 0) print } else print }' |
-rev | sort -r | sed -n "$FT_LINE1"','"$FT_LINE2"'p' |
+awk 'NR % 2 == 0' | rev | sort -r |
+sed -n "$FT_LINE1"','"$FT_LINE2"'p' |
 tr '\n' ' ' | sed -e 's/ /, /g ; s/, $/./'
