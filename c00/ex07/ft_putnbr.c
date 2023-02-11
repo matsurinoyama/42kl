179f6a2731d50ec82d5c6fdfb5208b5d1cf1b6f8
Azhan Rizan <matsurinoyama@pm.me>
2023-02-12 00:19:48 +0800

    Pushing changes, will finish the last exercise tomorrow

---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
---
+++ c00/ex08/ft_print_combn.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+void	ft_putchar(char a[])
+{
+	if (a[0] == '9' && a[1] == '8' && a[3] == '9' && a[4] == '9')
+	{
+		write(1, a, 5);
+		write(1, "\n", 1);
+	}
+	else
+		write(1, a, 7);
+}
+
+void    ft_print_combn(int n)
+{
+	
+    
+    char	a[7];
+
+	a[0], a[1], a[3], a[4] = '0';
+	a[2], a[6] = ' ';
+	a[5] = ',';
+	while (a[0] <= '9')
+	{
+		while (a[1] <= '8')
+		{
+			while (a[3] <= '9')
+			{
+				while (a[4] <= '9')
+				{
+					ft_putchar(a);
+					a[4]++;
+				}
+				a[3]++;
+			}
+			a[1]++;
+		}
+		a[0]++;
+	}
+}
+
+int main(void)
+{
+    ft_print_combn(2);
+}
