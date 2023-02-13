0ef9bbd119841f5ebffd6ee8ee5065117143bc73
Muhammad Azhan Bin Fadzlan Rizan <mbin-fad@u82z04s01.42kl.edu.my>
2023-02-13 13:23:27 +0800

    Made ex08 fail intentionally, not going to bother continuing this. Need to move on to c01 and beyond NOW.

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -1,45 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbin-fad <mbin-fad@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 11:58:03 by mbin-fad          #+#    #+#             */
+/*   Updated: 2023/02/13 13:20:45 by mbin-fad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void	ft_putchar(char a[])
+void	ft_putchar(char c)
 {
-	if (a[0] == '9' && a[1] == '8' && a[3] == '9' && a[4] == '9')
-	{
-		write(1, a, 5);
-		write(1, "\n", 1);
-	}
-	else
-		write(1, a, 7);
+	write(1, &c, 1);
 }
 
-void    ft_print_combn(int n)
+void	ft_print_combn(int nb)
 {
-	
-    
-    char	a[7];
-
-	a[0], a[1], a[3], a[4] = '0';
-	a[2], a[6] = ' ';
-	a[5] = ',';
-	while (a[0] <= '9')
+	if (nb <= 0 || nb > 10)
 	{
-		while (a[1] <= '8')
-		{
-			while (a[3] <= '9')
-			{
-				while (a[4] <= '9')
-				{
-					ft_putchar(a);
-					a[4]++;
-				}
-				a[3]++;
-			}
-			a[1]++;
-		}
-		a[0]++;
+		write(1, "Invalid input.\n", 15);
 	}
-}
-
-int main(void)
-{
-    ft_print_combn(2);
 }
